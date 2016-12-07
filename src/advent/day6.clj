e19c06113202284228038bb375effb39eb340db0
mnespor <mnespor@gmail.com>
2016-12-06 21:33:16 -0600

    style

--- src/advent/day6.clj
+++ src/advent/day6.clj
@@ -5,7 +5,15 @@
 (def input (-> "./resources/day6/input.txt" slurp string/split-lines))
 
 (defn solve [v]
-  (->> v core/transpose (map frequencies) (map #(sort-by val > %)) (map first)))
+  (->> v core/transpose
+       (map frequencies)
+       (map (partial sort-by val >))
+       (map ffirst)
+       (apply str)))
 
 (defn solve-2 [v]
-  (->> v core/transpose (map frequencies) (map #(sort-by val %)) (map first)))
+  (->> v core/transpose
+       (map frequencies)
+       (map (partial sort-by val))
+       (map ffirst)
+       (apply str)))
